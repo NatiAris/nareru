3737854710209955e71c01758873c8c5dba1a017
NatiAris <NatiAris@users.noreply.github.com>
2020-12-09 16:27:01 +0300

    Fix empty parens warning

--- app/controllers/CardmakerController.scala
+++ app/controllers/CardmakerController.scala
@@ -18,7 +18,7 @@ class CardmakerController @Inject()(val controllerComponents: ControllerComponen
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.cardmaker())
   }
 }
--- app/controllers/ReviewerController.scala
+++ app/controllers/ReviewerController.scala
@@ -18,7 +18,7 @@ class ReviewerController @Inject()(val controllerComponents: ControllerComponent
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index() = Action { implicit request: Request[AnyContent] =>
+  def index = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.reviewer())
   }
 }
